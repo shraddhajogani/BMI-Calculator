57b0145871c22248188b0f5edd2748dbec658413
Shraddha <jogani.shraddha@gmail.com>
2019-09-02 12:56:51 +0200

    To Simplify the code 1.Select that widget 2.Go to flutter outline( it selected in the widget tree there.) 3. right click and select extract widget. 4. Rename this Widget with Reusablecard 5.Remove New 5. Replace Container with new widget called Reusablecard card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
+
 
 void main() => runApp(BMICalculator());
 
@@ -6,10 +8,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.red,
-        accentColor: Colors.purple,
+      theme: ThemeData.dark().copyWith(
 
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
         
       ),
       home: InputPage(),
@@ -17,24 +19,4 @@ class BMICalculator extends StatelessWidget {
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
 
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
